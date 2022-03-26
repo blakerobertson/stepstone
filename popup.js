c2f824c1ca5b7f390792443031bc6524e853a25f
Blake Robertson <blaquer@gmail.com>
2022-03-26 01:23:06 -0400

    Changed to a node tree based approach to better deal with inline tags like links

--- popup.js
+++ popup.js
@@ -7,13 +7,35 @@ embolden.addEventListener("click", async () => {
 });
 
 function emboldenFirstLetters() {
-    let tags = document.body.getElementsByTagName("p");
-    for (let tag of tags) {
-        // TODO: improve this so it doesn't break the p's child tags
-        tag.innerHTML = tag.innerHTML.replace(/\b[a-zA-Z]+/g, s => {
-            let half = (s.length+1)/2;
-            return `<span style="font-weight:600">${s.slice(0, half)}</span>${s.slice(half)}`;
-        });
+    let ptags = document.body.getElementsByTagName("p");
+    for (let ptag of ptags) {
+        // capture the initial state of childNodes because we'll be changing it
+        const children = Array.from(ptag.childNodes);
+        for (let n of children) {
+            // n could be an inline element or something, so check its type
+            // note: TEXT_NODE type includes attribute values too (?!) so if we make this recursive later we'll need to account for that
+            if (n.nodeType === Node.TEXT_NODE) {
+                const words = Array.from(n.nodeValue.matchAll(/\b([a-zA-Z]+)/g));
+                console.log('words', words[0]);
+
+                for (let i=words.length-1; i>=0; i--) {
+                    let word_node = n.splitText(words[i].index);
+                    console.log("word_node", word_node.nodeValue);
+                    // n now contains everything before the matched word while
+                    // word_node is a new text node containing the word itself
+                    // AND ANY \b CHAR BEFORE IT AND ANY NON-ALPHA CHARS AFTER IT
+
+                    let halflength = (word_node.nodeValue.length+1)/2;
+                    let word_tail_node = word_node.splitText(halflength);
+                    // word_node is now the first half of the word and word_tail_node is the last half
+
+                    let span = document.createElement("span");
+                    span.appendChild(word_node);
+                    span.style.fontWeight = 600;
+                    ptag.insertBefore(span, word_tail_node);
+                }
+            }
+        }
     }
 }
 
